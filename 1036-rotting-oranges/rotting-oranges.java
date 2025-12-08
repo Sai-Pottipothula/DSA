018c1fc4baa6c2eff30aed3b5332b895ec5d0d3e
Sai Pottipothula <pbsn99@gmail.com>
2025-12-08 12:16:28 -0800

    Time: 1 ms (99.93%) | Memory: 44.2 MB (34.42%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+    Queue<int[]> queue = new LinkedList<>();
+
+    //first add all the rotten oranges in queue 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 2) {
+                queue.add(new int[]{i, j, 0});
+            }
+        }
+    }
+
+    //mark adjacent nodes as rotten and push in queue till it becomes empty
+    int maxMinutes = 0;
+    while (!queue.isEmpty()) {
+        int[] curr = queue.poll();
+        int x = curr[0], y = curr[1], level = curr[2];
+
+        if (x > 0 && grid[x - 1][y] == 1) {
+            grid[x - 1][y] = 2;
+            queue.add(new int[]{x - 1, y, level + 1});
+        }
+        if (x < m - 1 && grid[x + 1][y] == 1) {
+            grid[x + 1][y] = 2;
+            queue.add(new int[]{x + 1, y, level + 1});
+        }
+        if (y < n - 1 && grid[x][y + 1] == 1) {
+            grid[x][y + 1] = 2;
+            queue.add(new int[]{x, y + 1, level + 1});
+        }
+        if (y > 0 && grid[x][y - 1] == 1) {
+            grid[x][y - 1] = 2;
+            queue.add(new int[]{x, y - 1, level + 1});
+        }
+        maxMinutes = Math.max(level, maxMinutes);
+    }
+
+    // finally run over each element and check if any fresh is remaining 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 1) return -1;
+        }
+    }
+    return maxMinutes;
+    }
+}
