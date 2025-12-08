5d87396e6bb642d0c96554e8d3321d3ca6000947
Sai Pottipothula <pbsn99@gmail.com>
2025-12-08 12:18:15 -0800

    Time: 0 ms (100.00%) | Memory: 42.6 MB (31.39%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int temp=0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='R'){
+                temp++;
+            }
+            else{
+                temp--; 
+            }
+            if(temp==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
